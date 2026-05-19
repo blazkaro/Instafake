0d0d40b8c84259f6929e04bd07d2570ee7cb7783
Karol Błażejczyk <blazkaro@proton.me>
2026-05-19 23:48:31 +0200

    added missing properties for domain models

--- backend/src/Instafake.Posts.Domain/Entities/Post.cs
+++ backend/src/Instafake.Posts.Domain/Entities/Post.cs
@@ -7,9 +7,12 @@ public class Post
     public Guid Id { get; init; } = Guid.NewGuid();
     public required User Author { get; init; }
     public int LikesCount => _likedBy.Count;
-    public List<PostComment> Comments { get; } = [];
+    public List<string> ImageUrls { get; init; } = [];
+    public string Description { get; init; }
+    public List<string> Tags { get; init; } = [];
+    public DateTime CreatedAt { get; init; }
+    public List<PostComment> Comments { get; init; } = [];
 
     public bool Like(User user) => _likedBy.Add(user.Id);
     public bool Dislike(User user) => _likedBy.Remove(user.Id);
-    public void AddComment(PostComment comment) => Comments.Add(comment);
 }
