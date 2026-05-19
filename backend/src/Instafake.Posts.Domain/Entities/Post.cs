517d3689a04dc0918791484cc854dc09eba982e7
Karol Błażejczyk <blazkaro@proton.me>
2026-05-19 21:20:06 +0200

    get rid of unnecessary concurrent collections

--- backend/src/Instafake.Posts.Domain/Entities/Post.cs
+++ backend/src/Instafake.Posts.Domain/Entities/Post.cs
@@ -1,17 +1,15 @@
-﻿using System.Collections.Concurrent;
-
-namespace Instafake.Posts.Domain.Entities;
+﻿namespace Instafake.Posts.Domain.Entities;
 
 public class Post
 {
-    private readonly ConcurrentDictionary<string, byte> _likedBy = [];
+    private readonly HashSet<string> _likedBy = [];
 
     public Guid Id { get; init; } = Guid.NewGuid();
     public required User Author { get; init; }
     public int LikesCount => _likedBy.Count;
-    public ConcurrentBag<PostComment> Comments { get; } = [];
+    public List<PostComment> Comments { get; } = [];
 
-    public bool Like(User user) => _likedBy.TryAdd(user.Id, 0);
-    public bool Dislike(User user) => _likedBy.TryRemove(user.Id, out _);
+    public bool Like(User user) => _likedBy.Add(user.Id);
+    public bool Dislike(User user) => _likedBy.Remove(user.Id);
     public void AddComment(PostComment comment) => Comments.Add(comment);
 }
