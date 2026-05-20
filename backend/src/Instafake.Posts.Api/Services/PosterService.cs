0edb28b7fd535b6a1b4f22f5e7596834b84da29a
Karol Błażejczyk <blazkaro@proton.me>
2026-05-20 19:22:13 +0200

    prepared posts grpc api

---
+++ backend/src/Instafake.Posts.Api/Services/PosterService.cs
@@ -0,0 +1,22 @@
+﻿using Grpc.Core;
+using Instafake.Posts.Api.Protos;
+
+namespace Instafake.Posts.Api.Services;
+
+public class PosterService(ILogger<PosterService> logger) : Poster.PosterBase
+{
+    public override Task<CreatePostReply> CreatePost(CreatePostRequest request, ServerCallContext context)
+    {
+        return base.CreatePost(request, context);
+    }
+
+    public override Task<GetPostCommentsReply> GetPostComments(GetPostCommentsRequest request, ServerCallContext context)
+    {
+        return base.GetPostComments(request, context);
+    }
+
+    public override Task<GetPostsReply> GetPosts(GetPostsRequest request, ServerCallContext context)
+    {
+        return base.GetPosts(request, context);
+    }
+}
