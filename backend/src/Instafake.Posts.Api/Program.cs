cf933d0697ddcc8bcf308dda4a8f85c1075b07c7
Karol Błażejczyk <blazkaro@proton.me>
2026-05-20 17:21:13 +0200

    use grpc for posts api

--- backend/src/Instafake.Posts.Api/Program.cs
+++ backend/src/Instafake.Posts.Api/Program.cs
@@ -1,10 +1,14 @@
+using Instafake.Posts.Api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.AddServiceDefaults();
+// Add services to the container.
+builder.Services.AddGrpc();
+
 var app = builder.Build();
 
-app.MapDefaultEndpoints();
-
-app.MapGet("/", () => "Hello World!");
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
