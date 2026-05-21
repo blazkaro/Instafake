c8f58852d32ea939ba092832a14ab11215adc910
Karol Błażejczyk <blazkaro@proton.me>
2026-05-22 01:02:22 +0200

    test home page auth

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,16 @@
 import { Routes } from '@angular/router';
 import { SigninComponent } from './features/signin/signin-component';
+import { HomeComponent } from './features/home/home-component/home-component';
+import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
     {
         path: 'signin',
         component: SigninComponent
+    },
+    {
+        path: '',
+        component: HomeComponent,
+        canActivate: [authGuard]
     }
 ];
